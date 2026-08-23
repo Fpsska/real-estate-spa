accbfda08fb4bd7f7a6d4797c4d3335a4a0b779b
Fpsska <fpsska1337@gmail.com>
2026-08-23 17:43:38 +0300

    Remove previous cards module implementation

--- backend/src/card-templates/card-templates.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CardTemplatesController } from './card-templates.controller';
-import { CardTemplatesService } from './card-templates.service';
-
-@Module({
-    providers: [CardTemplatesService],
-    controllers: [CardTemplatesController]
-})
-export class CardTemplatesModule {}
