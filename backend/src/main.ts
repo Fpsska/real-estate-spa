1eaa2bf4d343d906e2208d9505ab2f0a351e5afa
Fpsska <fpsska1337@gmail.com>
2026-09-13 13:07:37 +0300

    Migrate to zero-config for vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,19 @@
-import { createApp } from './create-app';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await createApp();
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    app.enableCors({
+        origin: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+        allowedHeaders: 'Content-Type, Accept'
+    });
+    app.useGlobalPipes(new ValidationPipe());
+
     const PORT = process.env.PORT ?? 8080;
 
     await app.listen(PORT, () => {
